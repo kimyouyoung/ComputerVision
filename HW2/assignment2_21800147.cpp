f542c1957e8d9ca92620eb36eefebbd4f012c6cc
김유영 <21800147@handong.edu>
2020-09-08 15:35:06 +0900

    updated

--- HW2/assignment2_21800147.cpp
+++ HW2/assignment2_21800147.cpp
@@ -14,16 +14,17 @@ int main(){
     gray_img.convertTo(log_img,CV_32F);
     log_img = 1 + log_img;
     log(log_img, log_img);
-    log_img = log_img * 52;
-    convertScaleAbs(log_img, log_img); 
     normalize(log_img, log_img, 0, 255, NORM_MINMAX);
+    log_img.convertTo(log_img, CV_8U); 
+    log_img = log_img * 1.5;
     
 
-    gray_img.convertTo(gamma_img, CV_64F);
-    pow(gamma_img, 0.5, gamma_img);
-    gamma_img = gamma_img * 52;
-    convertScaleAbs(gamma_img, gamma_img); 
+    gray_img.convertTo(gamma_img, CV_32F);
     normalize(gamma_img, gamma_img, 0, 1.0, NORM_MINMAX);
+    pow(gamma_img, 0.5, gamma_img);
+    normalize(gamma_img, gamma_img, 0, 255, NORM_MINMAX);
+    gamma_img.convertTo(gamma_img, CV_8U); 
+    gamma_img = gamma_img * 1.1;
 
     imshow("gray_img", gray_img);
     imshow("negative_img", negative_img);
