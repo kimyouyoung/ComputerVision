8f842a767d253f93be73c857ab0267af258b0822
김유영 <21800147@handong.edu>
2020-09-25 16:45:00 +0900

    updated

--- HW4/assignment4_21800147.cpp
+++ HW4/assignment4_21800147.cpp
@@ -4,16 +4,16 @@
 using namespace cv;
 using namespace std;
 
-Mat drawHistogram(Mat src, int num_bins){
+Mat drawHistogram(Mat src){
     Mat hist, histImage;
 
     int hist_w, hist_h, bin_w, histSize;
-    float range[] = {0, 255};
+    float range[] = {0, 256};
     const float* histRange = { range };
 
     hist_w = 512;
     hist_h = 512;
-    histSize = num_bins;
+    histSize = 16;
     bin_w = cvRound((double)hist_w / histSize);
 
     histImage = Mat(hist_h, hist_w, CV_8U, Scalar(255, 255, 255));
@@ -29,6 +29,29 @@ Mat drawHistogram(Mat src, int num_bins){
     return histImage;
 }
 
+Mat drawHistogram_text(Mat src){
+    Mat hist;
+
+    int hist_w, hist_h, bin_w, histSize;
+    float range[] = {0, 256};
+    const float* histRange = { range };
+
+    hist_w = src.cols;
+    hist_h = src.rows;
+    histSize = 8;
+    bin_w = cvRound((double)hist_w / histSize);
+
+    calcHist(&src, 1, 0, Mat(), hist, 1, &histSize, &histRange);
+
+    for(int i = 0; i < histSize; i++){
+        hist.at<float>(i) = hist.at<float>(i)/(hist_w*hist_h);
+        string str = "bin " + to_string(i+1) + " : " + to_string(hist.at<float>(i));
+        putText(src, str, Point(5, 25 + (25*i)), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(255, 255, 255), 1);
+    }
+
+    return src;
+}
+
 int main(){
 
     Mat image, hist_equalized_image;
@@ -39,76 +62,15 @@ int main(){
     equalizeHist(image, hist_equalized_image);
 
     Mat hist_graph, hist_equalized_graph;
-    hist_graph = drawHistogram(image, 16);
-    hist_equalized_graph = drawHistogram(hist_equalized_image, 16);
+    hist_graph = drawHistogram(image);
+    hist_equalized_graph = drawHistogram(hist_equalized_image);
     imshow("h1", hist_graph);
     imshow("h2", hist_equalized_graph);
 
-    int one, two, three, four, five, six, seven, eight;
-    for(int i = 0; i < image.rows; i++){
-        for(int j = 0; j < image.cols; j++){
-            if(image.at<uchar>(i, j) >= 0 && image.at<uchar>(i, j) < 32)
-                one++;
-            else if(image.at<uchar>(i, j) >= 32 && image.at<uchar>(i, j) < 64)
-                two++;
-            else if(image.at<uchar>(i, j) >= 64 && image.at<uchar>(i, j) < 96)
-                three++;
-            else if(image.at<uchar>(i, j) >= 96 && image.at<uchar>(i, j) < 128)
-                four++;
-            else if(image.at<uchar>(i, j) >= 128 && image.at<uchar>(i, j) < 160)
-                five++;
-            else if(image.at<uchar>(i, j) >= 160 && image.at<uchar>(i, j) < 192)
-                six++;
-            else if(image.at<uchar>(i, j) >= 192 && image.at<uchar>(i, j) < 224)
-                seven++;
-            else
-                eight++;
-        }
-    }
 
-    string str, str2, str3, str4;
-    str = "1st bin: " + to_string((float)(one)/(image.cols*image.rows)) + ", 2nd bin: " + to_string((float)(two)/(image.cols*image.rows));
-    str2 = "3rd bin: " + to_string((float)(three)/(image.cols*image.rows)) + ", 4th bin: " + to_string((float)(four)/(image.cols*image.rows));
-    str3 = "5th bin: " + to_string((float)(five)/(image.cols*image.rows)) + ", 6th bin: " + to_string((float)(six)/(image.cols*image.rows));
-    str4 =  "7th bin: " + to_string((float)(seven)/(image.cols*image.rows)) + ", 8th bin: " + to_string((float)(eight)/(image.cols*image.rows)); 
-
-    putText(image, str, Point(5, 25), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    putText(image, str2, Point(5, 50), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    putText(image, str3, Point(5, 75), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    putText(image, str4, Point(5, 100), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    imshow("before", image);
-
-    for(int i = 0; i < hist_equalized_image.rows; i++){
-        for(int j = 0; j < hist_equalized_image.cols; j++){
-            if(hist_equalized_image.at<uchar>(i, j) >= 0 && hist_equalized_image.at<uchar>(i, j) < 32)
-                one++;
-            else if(hist_equalized_image.at<uchar>(i, j) >= 32 && hist_equalized_image.at<uchar>(i, j) < 64)
-                two++;
-            else if(hist_equalized_image.at<uchar>(i, j) >= 64 && hist_equalized_image.at<uchar>(i, j) < 96)
-                three++;
-            else if(hist_equalized_image.at<uchar>(i, j) >= 96 && hist_equalized_image.at<uchar>(i, j) < 128)
-                four++;
-            else if(hist_equalized_image.at<uchar>(i, j) >= 128 && hist_equalized_image.at<uchar>(i, j) < 160)
-                five++;
-            else if(hist_equalized_image.at<uchar>(i, j) >= 160 && hist_equalized_image.at<uchar>(i, j) < 192)
-                six++;
-            else if(hist_equalized_image.at<uchar>(i, j) >= 192 && hist_equalized_image.at<uchar>(i, j) < 224)
-                seven++;
-            else
-                eight++;
-        }
-    }
-
-    str = "1st bin: " + to_string((float)(one)/(hist_equalized_image.cols*hist_equalized_image.rows)) + ", 2nd bin: " + to_string((float)(two)/(hist_equalized_image.cols*hist_equalized_image.rows));
-    str2 = "3rd bin: " + to_string((float)(three)/(hist_equalized_image.cols*hist_equalized_image.rows)) + ", 4th bin: " + to_string((float)(four)/(hist_equalized_image.cols*hist_equalized_image.rows));
-    str3 = "5th bin: " + to_string((float)(five)/(hist_equalized_image.cols*hist_equalized_image.rows)) + ", 6th bin: " + to_string((float)(six)/(hist_equalized_image.cols*hist_equalized_image.rows));
-    str4 =  "7th bin: " + to_string((float)(seven)/(hist_equalized_image.cols*hist_equalized_image.rows)) + ", 8th bin: " + to_string((float)(eight)/(hist_equalized_image.cols*hist_equalized_image.rows)); 
-
-    putText(hist_equalized_image, str, Point(5, 25), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    putText(hist_equalized_image, str2, Point(5, 50), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    putText(hist_equalized_image, str3, Point(5, 75), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    putText(hist_equalized_image, str4, Point(5, 100), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 100, 200), 1);
-    imshow("after", hist_equalized_image);
+    imshow("before", drawHistogram_text(image));
+    
+    imshow("after", drawHistogram_text(hist_equalized_image));
 
     waitKey(0);
     return 0;
