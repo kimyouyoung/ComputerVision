3dabcb75c1e691a32087be7545dbe7b464a1a047
김유영 <21800147@handong.edu>
2020-10-29 12:15:21 +0900

    updated

---
+++ HW9/assignment9_21800147.cpp
@@ -0,0 +1,26 @@
+#include "cv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(){
+
+    Mat finger, adaptive_1, adaptive;
+    finger = imread("Finger.png", 0);
+    adaptive_1 = imread("adaptive_1.jpg", 0);
+    adaptive = imread("adaptive.png", 0);
+
+    threshold(finger, finger, 165, 255, THRESH_BINARY);
+
+    adaptiveThreshold(adaptive_1, adaptive_1, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 10);
+    
+    adaptiveThreshold(adaptive, adaptive, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 10);
+    
+    imshow("finger_print", finger);
+    imshow("adaptive_1", adaptive_1);
+    imshow("adaptive", adaptive);
+
+    waitKey(0);
+    return 0;
+}
